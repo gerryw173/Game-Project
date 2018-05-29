1d41fff256eb8bdfbc7d2fa5d7eaf3a839f49533
Gerry <31828746+gerryw173@users.noreply.github.com>
2018-05-28 22:40:04 -0400

    Add files via upload
    Added constructor and methods to Unit class.

--- src/Unit.java
+++ src/Unit.java
@@ -3,6 +3,55 @@ public abstract class Unit
     private int HP;
     private int DEF;
     private int ATK;
+    private int INT;
 
+    public Unit(int HP, int DEF, int ATK, int INT)
+    {
+        this.HP = HP;
+        this.DEF = DEF;
+        this.ATK = ATK;
+        this.INT = INT;
+    }
 
+    public int getHP()
+    {
+        return HP;
+    }
+
+    public void setHP(int HP)
+    {
+        this.HP = HP;
+    }
+
+    public int getDEF()
+    {
+        return DEF;
+    }
+
+    public void setDEF(int DEF)
+    {
+        this.DEF = DEF;
+    }
+
+    public int getATK()
+    {
+        return ATK;
+    }
+
+    public void setATK(int ATK)
+    {
+        this.ATK = ATK;
+    }
+
+    public int getINT()
+    {
+        return INT;
+    }
+
+    public void setINT(int INT)
+    {
+        this.INT = INT;
+    }
+
+    public abstract void attack();
 }
